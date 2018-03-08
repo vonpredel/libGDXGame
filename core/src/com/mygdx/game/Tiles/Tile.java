ce7327c1e772fb667cd1f207ac1e0ac95c468c2e
Rafal Predel <rpredel@si-ecommerce.pl>
2018-03-08 12:55:24 +0100

    MathUtils

--- core/src/com/mygdx/game/Tiles/Tile.java
+++ core/src/com/mygdx/game/Tiles/Tile.java
@@ -10,12 +10,14 @@ public abstract class Tile extends Rectangle {
 
     protected Texture texture;
     protected Color id;
+    protected int tileIndex;
 
     public Tile() {
         this.x = Constants.DEFAULT_START_POSITION_X;
         this.y = Constants.DEFAULT_START_POSITION_Y;
         this.height = Constants.DEFAULT_TILE_HEIGHT;
         this.width = Constants.DEFAULT_TILE_WIDTH;
+        this.tileIndex = -1;
     }
 
     public void draw(SpriteBatch batch) {
@@ -30,8 +32,4 @@ public abstract class Tile extends Rectangle {
         return false;
     }
 
-    // DO WYJEBANIA TYLKO DO TESTÓW
-    public void setTexture(Texture texture) {
-        this.texture = texture;
-    }
 }
---
+++ core/src/com/mygdx/game/Utils/MathUtils.java
@@ -0,0 +1,31 @@
+package com.mygdx.game.Utils;
+
+public final class MathUtils {
+
+    public static float EPSILON = 0.0001f;
+
+    private MathUtils() {
+
+
+    }
+
+    public static float lerp(float v0, float v1, float t) {
+        return v0 + (v1 - v0) * t;
+    }
+
+    public static float sign(float a) {
+        return a >= 0.0f ? 1.0f : -1.0f;
+    }
+
+    public static float moveTowards(float current, float target, float maxDelta) {
+        if (Math.abs(target - current) <= maxDelta)
+        {
+            return target;
+        }
+        return current + MathUtils.sign(target - current) * maxDelta;
+    }
+
+    public static boolean areEqual(float a, float b) {
+        return Math.abs(a - b) < EPSILON;
+    }
+}
